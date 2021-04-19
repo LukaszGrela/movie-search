ffbee13cb798021521067757919a7d63cfcd71c5
Lukasz Grela <lukasz.grela@gmail.com>
2021-04-19 22:19:14 +0100

    store search response in reducer

--- src/store/movies/slice.ts
+++ src/store/movies/slice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { IMoviesReducer } from "./types";
+import { searchMovies } from "./actions";
+import { guardIMovieErrorResponse, IMoviesReducer } from "./types";
 
 const initialState: IMoviesReducer = {
   loading: false,
@@ -10,6 +11,28 @@ const movies = createSlice({
   name: "movies",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(searchMovies.pending, (state, action) => {
+      state.loading = true;
+      state.error = undefined;
+      state.results = [];
+    });
+    builder.addCase(searchMovies.rejected, (state, action) => {
+      state.loading = false;
+      state.results = [];
+      state.error = action.error.message;
+    });
+    builder.addCase(searchMovies.fulfilled, (state, action) => {
+      state.loading = false;
+      if (guardIMovieErrorResponse(action.payload)) {
+        state.results = [];
+        state.error = action.payload.Error;
+      } else {
+        state.error = undefined;
+        state.results = action.payload.Search;
+      }
+    });
+  },
 });
 
 export default movies.reducer;
